fa5a646c87e1e59c1c7786eab53cef593699abb1
Favour Adekogbe <favouradekogbe@gmail.com>
2022-02-13 15:37:27 +0100

    set up default styles

---
+++ src/components/Login/index.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import styles from './styles.module.css';
+
+const LoginUI = () => {
+    return (
+        <div>
+            Login
+        </div>
+    )
+}
+
+export {LoginUI};
---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,10 @@
+import { LoginUI } from "components";
+import * as React from "react";
+
+const Login = () => {
+    return (
+        <LoginUI />
+    )
+}
+
+export default Login;
